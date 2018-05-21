2311b3b556ec3eb414f2bebfcc7cc6dcd5f66583
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-21 17:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays (string) {
-  return "Happy holidays!"
+  return 'Happy holidays!"
 }
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
